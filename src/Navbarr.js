319b491e96de58c2caabe9ebc7ec318c3263ebe8
Vinee th <peeksvinu@gmail.com>
2023-08-07 15:45:04 +0530

    deployment stage

--- src/Navbarr.js
+++ src/Navbarr.js
@@ -1,17 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function Navbarr() {
-    const nav = useNavigate();
+    // const nav = useNavigate();
 
     const welcome = () => {
         alert("WELCOME TO OUR PAGE")
     }
-    const Clear = () => {
-        nav(`/clear`)
-    }
+    // const Clear = () => {
+    //     nav(`/clear`)
+    // }
     return (
         <>
             <Navbar bg="blue" data-bs-theme="blue">
@@ -20,7 +20,7 @@ function Navbarr() {
                     <Nav className="me-auto">
                         {/* <Nav.Link><b>HOME</b></Nav.Link>   */}
                         <Nav.Link ><b>LANGUAGE-PICKER</b></Nav.Link>
-                        <Nav.Link onClick={Clear}><b>CLEAR</b></Nav.Link>
+                        {/* <Nav.Link onClick={Clear}><b>CLEAR</b></Nav.Link> */}
                     </Nav>
                 </Container>
             </Navbar>
